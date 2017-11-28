c9e092d1aeb3943ceac37d57f3712cad62636791
Aledj2 <Aledjones@nhs.net>
2017-11-28 15:22:34 +0000

    untested integrity checks - to be merged into dev_refactor branch before testing

--- automate_demultiplex_config.py
+++ automate_demultiplex_config.py
@@ -45,10 +45,13 @@ logfile_success = "Processing completed with 0 errors and 0 warnings."
 #path to upload agent
 upload_agent = "/home/mokaguys/Documents/apps/dnanexus-upload-agent-1.5.17-linux/ua"
 
-####################### Moka settings ######################
+
+# Moka settings 
+# variable used in sql update query
 moka_pipeline_ID="1941"
 
-########################## DNA Nexus setting#############################
+
+# DNA Nexus setting
 # project to upload run folder into
 NexusProjectPrefix="002_"
 
@@ -180,4 +183,10 @@ smtp_do_tls = True
 
 #####################Loggly
 # log command
-echo_to_log="echo %s 2>&1 | /usr/bin/logger -t %s"
+echo_to_log = "echo %s 2>&1 | /usr/bin/logger -t %s"
+
+##################### Integrity check
+# the filename which holds the checksum results
+md5checksum_name = "checksum_result.txt"
+# path to mapped miseq sequencer folders
+sequencer_share = {"M02353": "/media/M02353_MiSeqTemp/", "M02631": "/media/M02631_MiSeqTemp/"}
