6d4c099475295b830ed55b58c8d0b6c0845cce6b
Aledj2 <Aledjones@nhs.net>
2017-12-13 13:29:54 +0000

    change the verison to be in line with the releases on github

--- automate_demultiplex_config.py
+++ automate_demultiplex_config.py
@@ -9,7 +9,7 @@ The variables defined in this module are required by the "demultiplex.py" and
 debug = False
 
 # =====git release for the automate_demultiplexing repo=====
-script_release = "v1.0"
+script_release = "v5.0"
 
 # =====location of input/output files=====
 # path to run folders
