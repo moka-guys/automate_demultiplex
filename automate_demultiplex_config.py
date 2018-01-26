aebf3884c56d74c63d5658b0ab05ce25bf83557e
Aledj2 <Aledjones@nhs.net>
2018-01-26 13:37:15 +0000

    use config file for integrity check. repeat integrity check if fails. use new output from integrity check in demultiplexing.py

--- automate_demultiplex_config.py
+++ automate_demultiplex_config.py
@@ -5,11 +5,11 @@ The variables defined in this module are required by the "demultiplex.py" and
 "DNANexus_upload_agent.py" scripts.
 """
 # Set debug mode
-# Debug = True
+#debug = True
 debug = False
 
 # =====git release for the automate_demultiplexing repo=====
-script_release = "v9.0"
+script_release = "v10.0"
 
 
 # =====location of input/output files=====
@@ -225,10 +225,20 @@ smtp_do_tls = True
 # ================ Integrity check
 # the filename which holds the checksum results
 md5checksum_name = "md5checksum.txt"
-# path to mapped miseq sequencer folders
-sequencer_share = {"M02353": "/media/M02353_MiSeqTemp/", "M02631": "/media/M02631_MiSeqTemp/"}
 #checksum complete statement
-checksum_complete_flag="Checksum result reported"
+checksum_complete_flag = "Checksum result reported"
+# statement to write when checksums match
+checksum_match = "Checksums match"
+# hours to wait after RTAcomplete.txt file before first integrity check 
+integrity_check_first_wait = 3
+# hours between integrity checks
+integrity_check_repeat_wait = 1
+# maximum number of times to perform integrity test
+max_number_of_attempts = 10
+# list of files containing files which differ between temp and output
+missing_files_output = "missing_files.txt"
+#files to exclude from integrity check
+exclude = ["RTAStart.bat", "CorrectedIntMetrics.bin", "EmpiricalPhasingMetrics.bin", "ErrorMetrics.bin", "EventMetrics.bin", "ExtractionMetrics.bin", "PFGridMetrics.bin", "QMetrics.bin", "RegistrationMetrics.bin", "TileMetrics.bin", "000_000_000_na_rtabat.trans", "FilesAdded.csv", "FilesCopied.csv", "md5checksum.txt", missing_files_output]
 # ================ demultiplexing 
 logfile_success = "Processing completed with 0 errors and 0 warnings."
 
