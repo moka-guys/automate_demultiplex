fece39df2a41e4ab1051145c1175d6f379a8e202
Aledj2 <Aledjones@nhs.net>
2018-01-30 13:29:48 +0000

    fix the way the log files are combined

--- DNANexus_upload_agent.py
+++ DNANexus_upload_agent.py
@@ -61,35 +61,36 @@ class get_list_of_runs():
         # empty list
         list_of_logfiles = []
         #loop through the folder containing log files
-        for file in os.listdir(DNA_Nexus_workflow_logfolder):
+        for file in os.listdir(upload_agent_logfile):
             #if is one with this time stamp, ie if was made by this running of this script
-            if fnmatch.fnmatch(file, self.now + '*'):
+            if self.now in file:
                 #add count and append to list
-                count = count + 1
-                list_of_logfiles.append(DNA_Nexus_workflow_logfolder + file)
-        
+                count += 1
+                list_of_logfiles.append(upload_agent_logfile + file)
+
         #if more than one log file we want to concatenate them
         if count > 1:
             # create the start of the path to the logfile using the path to the log file and the time stamp (without.txt extension)
-            logfile_name = os.path.join(DNA_Nexus_workflow_logfolder, self.now)
+            logfile_name = os.path.join(upload_agent_logfile, self.now)
             # loop through all the log files to capture the run names 
             for logfile in list_of_logfiles:
                 #skip the empty timestamp
-                if self.now + ".txt" in logfile:
+                if self.now + "_.txt" in logfile:
                     pass
                 else:
                     # remove the time stamp and the logfolder path from each filename in the list and concatenate to the logfile_name created above
-                    logfile_name = logfile_name + logfile.replace(self.now,'').replace(DNA_Nexus_workflow_logfolder,'').replace(".txt","")
+                    logfile_name = logfile_name + logfile.replace(self.now, '').replace(upload_agent_logfile, '').replace(".txt", "").replace(".txt.txt","")
+            
             #add extension
-            logfile_name = logfile_name + "_uploadagent_log.txt"
+            logfile_name = logfile_name + ".txt"
 
             #concatenate all the remaining filenames into a string, seperated by spaces
             remaining_files = " ".join(list_of_logfiles)
             
             # combine all into one file with the longest filename (that will have the run folder name)            
             cmd = "cat " + remaining_files + " >> " + logfile_name
-            # remove the files that have been written to the longer file
-            rmcmd = "rm " + remaining_files
+            # remove the files that have been written to the longer file (removing the combined log file name from this list)
+            rmcmd = "rm " + remaining_files.replace(logfile_name,"")
                     
             # run the command, redirecting stderror to stdout
             proc = subprocess.call([cmd], stderr=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
@@ -132,8 +133,8 @@ class upload2Nexus():
         self.wes_number = '' # WES number near the end of fastq file name.
 
         #variables to rename log file.
-        self.rename=""
-        self.now=""
+        self.rename = ""
+        self.now = ""
 
         #####################################DNA Nexus########################
         # bash script that is used to execute dx commands
@@ -220,7 +221,7 @@ class upload2Nexus():
         self.now=now
         
         #open the logfile for this hour's cron job.
-        self.upload_agent_logfile_name=upload_agent_logfile+self.now+"_"+self.rename+".txt"
+        self.upload_agent_logfile_name = upload_agent_logfile + self.now + "_" + self.rename + ".txt"
         self.upload_agent_script_logfile = open(self.upload_agent_logfile_name,'a')
 
         # capture the runfolder 
@@ -475,7 +476,7 @@ class upload2Nexus():
         
         # server details
         server = smtplib.SMTP(host = host,port = port,timeout = 10)
-        server.set_debuglevel(1) # verbosity
+        server.set_debuglevel(False) # verbosity turned off - set to true to get debug messages
         server.starttls()
         server.ehlo()
         server.login(user, pw)
@@ -1005,12 +1006,16 @@ class upload2Nexus():
         self.upload_agent_script_logfile.close()
 
         # rename file to show what runs were affected.
-        self.rename=self.rename+self.runfolder
-        os.rename(self.upload_agent_logfile_name,self.upload_agent_logfile_name.replace('.txt','')+self.rename+".txt")
+        self.rename = self.runfolder + "_upload_agent_log.txt"
+        os.rename(self.upload_agent_logfile_name, self.upload_agent_logfile_name.replace('.txt', self.rename))
 
+        
         # capture the new file name so can continue to write to it.
-        self.upload_agent_logfile_name=self.upload_agent_logfile_name.replace('.txt','')+self.rename+".txt"
-
+        self.upload_agent_logfile_name = self.upload_agent_logfile_name.replace('.txt', self.rename)
+        
+        #reset self.rename to prevent logfile being renamed incorrectly.
+        self.rename = ""
+        
         # call function to upload log files
         self.upload_log_files()
 
@@ -1024,7 +1029,6 @@ class upload2Nexus():
         6. logfile used to set off the workflow (/home/mokaguys/Documents/automate_demultiplexing_logfiles/DNA_Nexus_workflow_logs)
         7. samplesheet
         '''
-
         #empty list to hold files (and paths) to be uploaded
         logfile_list=[]
         
@@ -1191,8 +1195,6 @@ class upload2Nexus():
             else:
                 #write to log file check was ok
                 self.logger("upload of fastq files complete for run "+self.runfolder,"UA_pass")
-
-
 
 
     def look_for_upload_errors_runfolder(self):
