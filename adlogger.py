45bdc3b29872bd666f02d5b2052b6148c3c92859
Aledj2 <Aledjones@nhs.net>
2020-02-07 14:19:36 +0000

    Update logging docstring for get_runfolder_log_config and DataOnlyLogger

--- adlogger.py
+++ adlogger.py
@@ -1,11 +1,28 @@
-import os
-import automate_demultiplex_config as config
+"""adlogger.py
+
+Automate demultiplex logging.
+"""
 import logging
 import logging.handlers
+import os
+
+import automate_demultiplex_config as config
 
 
 def get_runfolder_log_config(runfolder, timestamp):
-    #  Demultiplex script log has timestamp that differs from current time. Find first.
+    """Return an ADLogger config dictionary for a given runfolder.
+
+    Args:
+        runfolder(upload_and_setoff_workflows.RunfolderObject): A runfolder
+        timestamp(str): Timestamp produced by the datetime module.
+            E.g. str("{:%Y%m%d_%H%M%S}".format(datetime.datetime.now()))
+    Returns:
+        log_config(dict): A dictionary of arguments for ADLoggers
+    """
+
+    # Find the demultiplex logfile for the runfolder.
+    # This logfile is named with the timestamp of demultiplexing and the runfolder. We cannot know
+    # the timestamp so search for any demultiplex logfiles matching the runfodler name and return.
     any_demultiplex_logs = [
         os.path.join(config.demultiplex_logfiles, filename)
         for filename in os.listdir(config.demultiplex_logfiles)
@@ -13,22 +30,35 @@ def get_runfolder_log_config(runfolder, timestamp):
     ]
     demultiplex_log = any_demultiplex_logs.pop() if any_demultiplex_logs else None
 
-    # Configuration for ADLoggers. Provides mapping between logger shorthand name and the logfile filepath.
+    # Configuration for ADLoggers.
+    # Key,value pairs where keys are ADLoggers init arguments and values are logfile paths.
     log_config = {
         "script": os.path.join(config.upload_agent_logfile, timestamp + "_.txt"),
         "project": os.path.join(
-            config.DNA_Nexus_project_creation_logfolder + runfolder.runfolder_name + ".sh"
+            config.DNA_Nexus_project_creation_logfolder
+            + runfolder.runfolder_name
+            + ".sh"
         ),
         "dx_run": runfolder.runfolder_dx_run_script,
         "demultiplex": demultiplex_log,
-        "fastq_upload": os.path.join(runfolder.runfolderpath, config.upload_started_file),
-        "backup": os.path.join(config.backup_runfolder_logfile, runfolder.runfolder_name + ".log",),
+        "fastq_upload": os.path.join(
+            runfolder.runfolderpath, config.upload_started_file
+        ),
+        "backup": os.path.join(
+            config.backup_runfolder_logfile, runfolder.runfolder_name + ".log",
+        ),
     }
 
     return log_config
 
 
-class LOGFILE:
+class DataOnlyLogger:
+    """Carry metadata for logfiles that are not written to.
+
+    Args:
+        name(str): Logfile shorthand name
+        filepath(str): Logfile filepath
+    """
     def __init__(self, name, filepath):
         self.name = name
         self.filepath = filepath
@@ -36,10 +66,12 @@ class LOGFILE:
 
 class ADLoggers:
 
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
 
     def __init__(self, project, dx_run, demultiplex, fastq_upload, backup, script=None):
-        # Logfiles the demultiplex script writes to
+        # Logfiles the demultiplex script writes to. Createed im
         self.script = self._get_ad_logger("automate_demultiplex", script)
         self.backup = self._get_ad_logger("backup_runfolder", backup)
         self._fastq_upload = (
@@ -49,10 +81,18 @@ class ADLoggers:
         #  creation but still provides convenience attributes .name and .filename
         self.project = self._get_ad_logger("create_project", project, file_only=True)
         self.dx_run = self._get_ad_logger("dx_run", dx_run, file_only=True)
-        self.demultiplex = self._get_ad_logger("demultiplex", demultiplex, file_only=True)
+        self.demultiplex = self._get_ad_logger(
+            "demultiplex", demultiplex, file_only=True
+        )
 
         # Container for all logfiles
-        self.all = [self.script, self.project, self.dx_run, self.demultiplex, self.backup]
+        self.all = [
+            self.script,
+            self.project,
+            self.dx_run,
+            self.demultiplex,
+            self.backup,
+        ]
 
     def set_fastq_upload(self):
         self.fastq_upload = self._get_ad_logger("fastq_upload", self._fastq_upload)
@@ -76,7 +116,7 @@ class ADLoggers:
     def _get_ad_logger(self, name, filepath, file_only=False):
         """Returns automate demultiplex logger"""
         if file_only or filepath is None:
-            return LOGFILE(name, filepath)
+            return DataOnlyLogger(name, filepath)
         logger = logging.getLogger(name)
         logger.filepath = filepath
         logger.setLevel(logging.DEBUG)
@@ -87,6 +127,7 @@ class ADLoggers:
 
 if __name__ == "__main__":
     loggers = ADLoggers(None, None, None, None, None, script="test.log")
+    loggers.set_fastq_upload()
     print("Writing test to {} and syslog".format(loggers.script.filepath))
     loggers.script.info("This is a test")
     for logger in loggers.all:
