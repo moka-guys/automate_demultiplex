de8bfd1ba90548de0a2d55f9d88339c5f567e516
Aledj2 <Aledjones@nhs.net>
2020-03-02 17:50:48 +0000

    Add comments test for runfolder processor

--- test/test_app.py
+++ test/test_app.py
@@ -1,9 +1,9 @@
-"""Test upload_and_setoff_workflows.py by mocking external calls"""
+"""Test upload_and_setoff_workflows.py"""
 
-# This application is not a python package. Add parent directory to path to import modules
 import sys
 import smtplib
 import os
+# This application is not a python package. Add parent directory to sys path to import custom modules
 sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
 )
@@ -12,12 +12,24 @@ import upload_and_setoff_workflows as uasw
 from upload_and_setoff_workflows import RunfolderProcessor
 from mocks import MockRunfolderProcessor, mock_rename, mock_email
 
-def test_app(monkeypatch):
-    # Patch Runfolder processor to mock out external calls and file moves.
+def test_runfolder_processor(monkeypatch):
+    """Test upload_and_setoff_workflows.py protocol while replacing functions in
+    uasw.RunfolderProcessor with those in MockRunfolderProcessor.
+
+    As RunfolderProcessor carries most methods used to process NGS runfolders, this test suite allows
+    us to test specific functions while skipping expensive operations like creating runfolders,
+    running the NGS workflows or sending emails.
+    """
+
+    # Patch RunfolderProcessor to use any functions specified in MockRunfolderProcessor
     monkeypatch.setattr(uasw, "RunfolderProcessor", MockRunfolderProcessor)
+    # Patch os.rename to skip moving logfiles
     monkeypatch.setattr(os, "rename", mock_rename)
+    # Patch smtplib to stop emails being sent
     monkeypatch.setattr(smtplib.SMTP, "sendmail", mock_email)
 
+    # Run the protocol for processing NGS runfolders. This is equivalent to:
+    # `python upload_and_setoff_workflows.py`
     runs = uasw.SequencingRuns()
     runs.set_runfolders()
     runs.loop_through_runs()
