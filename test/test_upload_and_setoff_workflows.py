7dd8336ac3779f639a940b6ac5d5c17839c64bd8
Aledj2 <Aledjones@nhs.net>
2020-12-08 16:15:00 +0000

    add test script for pytest

---
+++ test/test_upload_and_setoff_workflows.py
@@ -0,0 +1,82 @@
+import sys
+import smtplib
+import unittest
+#import pytest
+from _pytest.monkeypatch import MonkeyPatch
+
+from mock import mock, patch
+ 
+
+import os
+# This application is not a python package. Add parent directory to sys path to import custom modules
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+)
+import automate_demultiplex_config_test as config
+import mock_inputs_and_outputs as mio
+
+
+from mocks import MockRunfolderProcessor, mock_rename, mock_email
+
+# This mock points all calls to automate_demultiplex_config to 
+# automate_demultiplex_config_test to allow cloud based testing 
+with mock.patch.dict('sys.modules', automate_demultiplex_config= config):
+   import upload_and_setoff_workflows as uasw
+   from upload_and_setoff_workflows import RunfolderProcessor, RunfolderObject
+
+
+'''
+    If you wish to patch a single method from a class OR patch an entire class 
+    use the @patch
+    # this works for mocking the whole class
+    # @patch('test_upload_and_setoff_workflows.RunfolderProcessor', MockRunfolderProcessor) 
+    # patch star_building_dx_run_cmds method only with a mock
+    #@patch.object(RunfolderProcessor, 'start_building_dx_run_cmds', mock_start_building_dx_run_cmds) 
+'''
+
+
+class TestUploadandSetoffWorkflows(unittest.TestCase):
+
+
+    def setUp(self):
+    # Enable usage of monkeypatched classes across all unit tests in this test class 
+        self.monkeypatch = MonkeyPatch()
+
+        # unhash to see the full diff in command line  
+        #self.maxDiff = None
+
+    def test_start_building_dx_run_cmds(self):
+            
+        now = "2018-03-29 10:26:23.473031"
+        # create an instance of RunfolderProcessor
+        # Using the testing folder run below
+        folder = '999999_NB552085_0077_AHYNCMAFXY'
+        # set monkeypatch to change the pointer of RunfodlerProcessor to MockRunfolderProcessor 
+        # which is in the mocks script
+        # any methods define in MockRunfolderProcessor will be taken from there
+        # any methods not defined in there will be taken from RunfolderProcessor as 
+        # it is called in this script
+        self.monkeypatch.setattr(__name__+  '.RunfolderProcessor', MockRunfolderProcessor)
+        test_runfolder_instance =  RunfolderProcessor(folder, now, debug_mode=config.debug)
+        # run the test input from mio
+        expected_list = test_runfolder_instance.start_building_dx_run_cmds(mio.list_of_processed_samples)
+        # check if output of method matches known true output
+        #print('this is expected list')
+        #print(expected_list)
+        self.assertEqual (expected_list, mio.test_output_commands_list)
+        #assert expected_list == mio.test_output_commands_list
+
+
+
+'''     
+    def test_find_fastqs(self):
+        # defining variables 
+        now = "2018-03-29 10:26:23.473031"
+        #create an instance of RunfolderProcessor
+        test_runfolder_instance =  RunfolderProcessor(config.runfolders, now, debug_mode=config.debug)
+        expected_list = test_runfolder_instance.find_fastqs(mio.runfolder_fastq_path)
+        # find_fastqs takes one input but returns two outputs 
+        #self.assertEqual (expected_list, mio.list_of_processed_samples)
+        self.assertEqual (expected_list, mio.expected_output_find_fastq)
+'''
+
