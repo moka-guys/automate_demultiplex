1ae5eebccf2ccb7612829610a449f33c829d3c32
mokaguys <rachel.g.duffin@gmail.com>
2023-05-18 14:13:48 +0100

    Fix runfolder_obj samplesheet path, alter import paths, add dx run script paths to runfolder object, add runfolder demultiplex and upload script logfiles to runfolder object

--- runfolder_obj/runfolder_obj.py
+++ runfolder_obj/runfolder_obj.py
@@ -1,71 +1,120 @@
 # coding=utf-8
-""" rf_obj.py
+""" runfolder_obj.py
 
 This module creates an object with runfolder specific properties
 """
 import os
-import ad_config as config
+import config.ad_config as ad_config
 
+# TODO make this runnable on the command line
 
 class RunfolderObject(object):
     """
-    An object with runfolder specific properties.
+    An object with runfolder-specific properties.
+
+    Args:
+        runfolder_name (str):   Runfolder name string
+        timestamp (str):        Timetamp in the format
+                                str(f"{datetime.datetime.now():%Y%m%d_%H%M%S}")
     """
 
     def __init__(self, runfolder_name, timestamp):
         self.timestamp = timestamp
-        # Set empty variables to be defined based on the run
         self.runfolder_name = runfolder_name
         self.runfolderpath = os.path.join(
-            config.RUNFOLDERS, self.runfolder_name
+            ad_config.RUNFOLDERS, self.runfolder_name
         )
-        # Sequencing finished file
+        self.samplesheet_name = (
+            ad_config.SAMPLESHEET_NAME % self.runfolder_name
+            )
+        # Stored within runfolder
+        # Sequencing finished file (within runfolder)
         self.rtacompletefile_path = os.path.join(
-            self.runfolderpath, config.FILENAMES["rtacomplete"]
+            self.runfolderpath, ad_config.FILENAMES["rtacomplete"]
         )
-        # Samplesheet
-        self.samplesheet_name = config.SAMPLESHEET_NAME % self.runfolder_name
-        self.samplesheet_path = config.SAMPLESHEET_PATH % self.runfolder_name
+        # Samplesheet (within runfolder)
         self.runfolder_samplesheet_path = os.path.join(
             self.runfolderpath, self.samplesheet_name
         )
-
-        # Integrity check file
+        # Integrity check file (within runfolder)
         self.checksumfile_path = os.path.join(
-            self.runfolderpath, config.FILENAMES["md5checksum"]
+            self.runfolderpath, ad_config.FILENAMES["md5checksum"]
         )
-        # Bcl2fastq log file
+        # Bcl2fastq log file (within runfolder)
         self.bcl2fastqlog_path = os.path.join(
-            self.runfolderpath, config.FILENAMES["bcl2fastqlog"]
+            self.runfolderpath, ad_config.FILENAMES["bcl2fastqlog"]
         )
-        # Project fastq folder
+        # Runfolder fastq folder (within runfolder)
         self.fastq_dir_path = os.path.join(
-            self.runfolderpath, config.DIRS["fastqs"]
+            self.runfolderpath, ad_config.DIRS["fastqs"]
         )
-        # Runfolder dx run commands
-        self.runfolder_dx_run_script = config.DXRUN_SCRIPT % runfolder_name
-        self.congenica_upload_cmds_file = (
-            config.PATHS["congenica_upload_script"] % self.runfolder_name
-        )
-        # Dnanexus project creation logfile
-        self.project_creation_logfile = (
-            config.PROJ_CREATION_SCRIPT % self.runfolder_name
-        )
-        # Backup runfolder logfile
-        self.backup_runfolder_logfile = (
-            config.BACKUP_RUNFOLDER_LOGFILE % self.runfolder_name
-        )
-        # Upload agent logfile
+        # Upload agent logfile (within runfolder)
         self.upload_agent_logfile = os.path.join(
-            self.runfolderpath, config.FILENAMES["upload_started"]
+            self.runfolderpath, ad_config.FILENAMES["upload_started"]
         )
-
+        # Bcl2fastq stats file (within runfolder)
         self.bcl2fastqstats_file = os.path.join(
             self.runfolderpath,
-            config.DIRS["bcl2fastq_stats"],
-            config.FILENAMES["bcl2fastq_stats"],
+            ad_config.DIRS["bcl2fastq_stats"],
+            ad_config.FILENAMES["bcl2fastq_stats"],
         )
 
+        # Stored within samplesheets dir
+        # Samplesheet (samplesheets dir)
+        self.samplesheet_path = (
+            ad_config.SAMPLESHEET_PATH % self.runfolder_name
+            )
+
+        # Stored within logfiles dir
+        # Workflow dx run commands for runfolder (within logfiles dir)
+        self.runfolder_dx_run_script = (
+            ad_config.LOGFILES['dx_run_script'] % self.runfolder_name
+            )
+        # Congenica upload commands for runfolder (within logfiles dir)
+        self.congenica_dx_run_script = (
+            ad_config.LOGFILES["congenica_upload_script"] % self.runfolder_name
+        )
+        # Dnanexus project creation bash script (within logfiles dir)
+        self.project_creation_logfile = (
+            ad_config.LOGFILES["proj_creation_script"] % self.runfolder_name
+        )
+        # Backup runfolder logfile (within logfiles dir)
+        self.backup_runfolder_logfile = (
+            ad_config.LOGFILES["backup_runfolder"] % self.runfolder_name
+        )
+        # Logfiles that contain timestamps - uses get_scriptlog() to search
+        # for existing logfile containing runfolder name
+
+        # Logfile to contain runfolder upload log
+        self.upload_runfolder_logfile = self.get_runfolder_logs(
+            ad_config.LOGDIRS["upload_script"],
+            ad_config.LOGFILES["upload_script"] %
+            f"{self.timestamp}_{self.runfolder_name}"
+            )
+        # Logfile to contain runfolder demultiplex log
+        self.demultiplex_runfolder_logfile = self.get_runfolder_logs(
+            ad_config.LOGDIRS["demultiplex"],
+            ad_config.LOGFILES["demultiplex_script_logfile"] %
+            f"{self.timestamp}_{self.runfolder_name}"
+            )
+
+    def get_runfolder_logs(self, directory, logfile):
+        """
+        Find the the logfile for the runfolder. Logfile contains an unknown
+        timestamp. Search for any demultiplex logfiles matching the runfolder
+        name and return the first
+        If none exist, get the logfile from before it is renamed with
+        processed runfolders
+        """
+        any_logs = [
+            os.path.join(directory, filename)
+            for filename in os.listdir(directory)
+            if self.runfolder_name in filename
+        ]
+        logfile = any_logs.pop() if any_logs else logfile
+        return logfile
+
+        # TODO take these out of this class
         self.nexus_project_name = ""
         self.nexus_path = ""
         self.nexus_project_id = ""
