b09e7aee7928f8a0360e2711e1dc06183e1ddc2d
mokaguys <rachel.g.duffin@gmail.com>
2023-07-18 10:24:56 +0100

    Rename shared_functions as toolbox

--- toolbox/toolbox.py
+++ toolbox/toolbox.py
@@ -4,13 +4,59 @@
 This script contains functions shared across scripts / modules
 """
 import os
-import re
 import subprocess
 import logging
+import argparse
 from distutils.spawn import find_executable
 from typing import Union
-import config.ad_config as ad_config
-import ad_logger.ad_logger as ad_logger
+from config import ad_config
+from ad_logger import ad_logger
+
+
+# TODO improve README documentation
+
+
+def script_start_logmsg(logger, file):
+    """"""
+    logger.info(
+        logger.log_msgs["script_start"], git_tag(),
+        os.path.basename(os.path.dirname(file))
+    )
+
+
+def script_end_logmsg(logger, file):
+    """"""
+    logger.info(
+        logger.log_msgs["script_end"],
+        git_tag(),
+        os.path.basename(os.path.dirname(file))
+    )
+
+
+def is_valid_dir(parser: argparse.ArgumentParser, arg: str) -> str:
+    """
+    Check directory path is valid
+        :param parser (argparse.ArgumentParser):    Holds necessary info to parse cmd
+                                                    line into Python data types
+        :param arg (str):                           Input argument
+    """
+    if not os.path.isdir(arg):
+        parser.error(f"The directory {arg} does not exist!")
+    else:
+        return arg  # Return argument
+
+
+def is_valid_file(parser: argparse.ArgumentParser, arg: str) -> str:
+    """
+    Check file path is valid
+        :param parser (argparse.ArgumentParser):    Holds necessary info to parse cmd
+                                                    line into Python data types
+        :param arg (str):                           Input argument
+    """
+    if not os.path.exists(arg):
+        parser.error(f"The file {arg} does not exist!")
+    else:
+        return arg  # Return argument
 
 
 def git_tag():
@@ -39,11 +85,7 @@ def execute_subprocess_command(command: str, logger: logging.Logger) -> (str, st
         stderr(str),
         returncode(int))(tuple):        Outputs from the command
     """
-    logger.info(
-        logger.log_msgs["executing_command"],
-        command,
-        extra={"flag": logger.log_flags["info"] % "usw"},
-    )
+    logger.info(logger.log_msgs["executing_command"], command)
     proc = subprocess.Popen(
         [command],
         stderr=subprocess.PIPE,
@@ -72,17 +114,9 @@ def check_returncode(proc: subprocess.Popen, logger: object) -> (str, str, int):
     returncode = proc.returncode
 
     if returncode == 0:
-        logger.info(
-            logger.log_msgs["cmd_success"],
-            returncode,
-            extra={"flag": logger.log_flags["info"] % "usw"},
-        )
+        logger.info(logger.log_msgs["cmd_success"], returncode)
     else:
-        logger.exception(
-            logger.log_msgs["cmd_fail"],
-            returncode, out, err,
-            extra={"flag": logger.log_flags["fail"] % "usw"},
-        )
+        logger.error(logger.log_msgs["cmd_fail"], returncode, out, err)
     return out, err, returncode
 
 
@@ -90,6 +124,30 @@ def get_runfolder_path(runfolder_name):
     """
     """
     return os.path.join(ad_config.RUNFOLDERS, runfolder_name)
+
+
+def test_upload_software(logger) -> Union[bool, None]:
+    """
+    Test the required software is installed and performing
+        :return True | None:    Return True if all software tests pass, else None
+    """
+    if test_programs("dx_toolkit", logger) and test_programs("upload_agent", logger):
+        return True
+    else:
+        logger.error(logger.log_msgs["software_fail"])
+        raise Exception
+
+
+def test_processing_software(logger) -> Union[bool, None]:
+    """
+    Test the software is installed and performing, by calling the test_upload_agent
+    and test_dx_toolkit functions
+        :return True|None:  Return true if the tests all pass
+    """
+    if test_programs("bcl2fastq2", logger) and test_programs(
+        "gatk_collect_lane_metrics", logger
+    ):
+        return True
 
 
 def test_programs(software_name: str, logger: object) -> Union[bool, None]:
@@ -101,41 +159,40 @@ def test_programs(software_name: str, logger: object) -> Union[bool, None]:
         :return True | None:              Return True if test passes, else return None
     """
     software_dict = ad_config.TEST_PROGRAMS_DICT[software_name]
-    logger.info(
-        logger.log_msgs["testing_software"],
-        "bcl2fastq2",
-        extra={"flag": logger.log_flags["info"] % "usw"},
-    )
+
+    logger.info(logger.log_msgs["testing_software"], software_name)
+
     if find_executable(software_dict["executable"]):
-        logger.info(
-            logger.log_msgs["found_program"],
-            software_dict['executable'],
-            extra={"flag": logger.log_flags["info"] % "usw"},
-            )
+        logger.info(logger.log_msgs["found_program"], software_dict['executable'])
         out, err, returncode = execute_subprocess_command(
             software_dict["test_cmd"], logger
             )
         if returncode == 0:
-            logger.info(
-                logger.log_msgs["test_pass"],
-                software_name,
-                extra={"flag": logger.log_flags["info"] % "usw"},
-            )
+            logger.info(logger.log_msgs["test_pass"], software_name)
             return True
         else:
-            logger.exception(
-                logger.log_msgs["test_fail"],
-                software_name,
-                extra={"flag": logger.log_flags["fail"] % "usw"},
-            )
+            logger.error(logger.log_msgs["test_fail"], software_name)
             raise Exception  # Stop script
     else:
-        logger.exception(
-            logger.log_msgs["program_missing"],
-            software_dict['executable'],
-            extra={"flag": logger.log_flags["fail"] % "usw"},
-        )
-        raise Exception
+        logger.error(logger.log_msgs["program_missing"], software_dict['executable'])
+        raise Exception  # Stop script
+
+
+def return_scriptlogfile(logger_name):
+    """
+    Return 
+    """
+    SCRIPT_LOGS = {
+        'demultiplex': os.path.join(  # Record demultiplex script logs
+            ad_config.AD_LOGDIR, "demultiplexing_script_logfiles",
+            f"{ad_config.TIMESTAMP}_demultiplex_script_log.log"
+            ),
+        'usw': os.path.join(  # Record usw script logs
+            ad_config.AD_LOGDIR, "usw_script_logfiles",
+            f"{ad_config.TIMESTAMP}_upload_and_setoff_workflow.log"
+        ),
+    }
+    return SCRIPT_LOGS[logger_name]
 
 
 def return_rflog_config(runfoldername):
@@ -161,20 +218,38 @@ def return_rflog_config(runfoldername):
             ad_config.AD_LOGDIR, "dx_run_commands",
             f"{runfoldername}_dx_run_commands.sh"
             ),
-        "congenica_upload_script": os.path.join(  # DNAnexus run command script
+        "congenica_dx_run_script": os.path.join(  # DNAnexus run command script
             ad_config.AD_LOGDIR, "dx_run_commands",
             f"{runfoldername}_congenica.sh"
             ),
         # Script containing dnanexus project creation command
         "proj_creation_script": os.path.join(
             ad_config.AD_LOGDIR, "nexus_project_creation_scripts",
-            f"create_nexus_project_{runfoldername}.sh"
+            f"{runfoldername}_create_nexus_project.sh"
             ),
         "decision_support_tool_logfile": os.path.join(
             ad_config.AD_LOGDIR, "decision_support_script_logfiles",
-            f"decision_support_script_log_{runfoldername}.log"
+            f"{runfoldername}_decision_support_script_log.log"
             ),
+        "samplesheet_validator_logfile": os.path.join(
+            ad_config.AD_LOGDIR, "samplesheet_validator_script_logfiles",
+            f"{runfoldername}_samplesheet_validator_script_log.log"
+        )
     }
+
+
+def get_num_processed_runfolders(logger, script_name, processed_runfolders):
+    """
+    Set self.num_processed_runfolders
+        :return num_processed_runfolders (int): Number of processed runfolders
+    """
+    num_processed_runfolders = len(processed_runfolders)
+    logger.info(
+        logger.log_msgs["runfolders_processed"],
+        num_processed_runfolders,
+        ", ".join(processed_runfolders),
+    )
+    return num_processed_runfolders
 
 
 class RunfolderObject(object):
@@ -204,50 +279,42 @@ class RunfolderObject(object):
         bcl2fastqstats_file (str):              Bcl2fastq stats file (within runfolder)
         samplesheet_path (str):                 Runfolder samplesheet (within
                                                 samplesheets dir)
+        demultiplex_runfolder_logfile (str):    Runfolder demultiplex logfile (within
+                                                logfiles dir)
+        upload_runfolder_logfile (str):         Runfolder upload and setoff workflows
+                                                logfile (within logfiles dir)
+        backup_runfolder_logfile (str):         Backup runfolder logfile (within
+                                                logfiles dir)
         runfolder_dx_run_script (str):          Workflow dx run commands for runfolder
                                                 (within logfiles dir)
         congenica_dx_run_script (str):          Congenica upload commands for runfolder
                                                 (within logfiles dir)
         proj_creation_script (str):             DNAnexus project creation bash script
                                                 (within logfiles dir)
-        backup_runfolder_logfile (str):         Backup runfolder logfile (within
-                                                logfiles dir)
         decision_support_tool_logfile (str):    Decision support tool inputs script
                                                 logfile (within logfiles dir)
-        upload_runfolder_logfile (str):         Runfolder upload and setoff workflows
-                                                logfile (within logfiles dir)
-        demultiplex_runfolder_logfile (str):    Runfolder demultiplex logfile (within
-                                                logfiles dir)
+        samplesheet_validator_logfile (str):    Samplesheet validator script logfile
+                                                (within logfiles dir)
         cluster_density_files (list):           List containing runfolder lane metrics
                                                 and phasing metrics file paths
         logfiles_config (dict):                 Contains all runfolder log files
-        script_loggers (object):                AdLoggers object with script-level
-                                                loggers
         logfiles_to_upload (list):              All logfiles that require upload to
                                                 DNAnexus
-        loggers (object):                       AdLogger object containing
+        logfile (path):                         One per runfolder logfile
+        rf_loggers (object):                    RunfolderLoggers object containing
                                                 runfolder-specific loggers
 
     Methods
-        requires_processing()
-            Calls other methods to determine whether the runfolder requires processing
-            (demultiplexing has finished successfully and the runfolder has not already
-            been uploaded)
-        already_uploaded()
-            Checks for presence of upload agent logfile (denotes that the runfolder has
-            already been processed).
-        has_demultiplexed()
-            Check if demultiplexing has already been performed and completed sucessfully
+        set_rf_logfiles()
+            Add runfolder log files as class atributes
         add_runfolder_loggers()
             Add runfolder loggers to runfolder object
     """
 
-    def __init__(self, runfolder_name: str, script_logger: object, timestamp: str):
+    def __init__(self, runfolder_name: str, timestamp: str):
         """
         Constructor for the RunfolderObject class
             :param runfolder_name (str):    Name of runfolder
-            :param script_loggers (object): Script-level logger object with log_msgs 
-                                            and log_flags attributes
             :param timestamp (str):         Timestamp in the format
                                             str(f"{datetime.datetime.now():
                                             %Y%m%d_%H%M%S}")
@@ -310,130 +377,39 @@ class RunfolderObject(object):
             "backup": self.backup_runfolder_logfile,
             "project": self.proj_creation_script,
             "dx_run": self.runfolder_dx_run_script,
+            "decision_support": self.decision_support_tool_logfile,
+            "ss_validator": self.samplesheet_validator_logfile,
         }
-        self.script_logger = script_logger
         self.logfiles_to_upload = [
             self.upload_runfolder_logfile,
             self.demultiplex_runfolder_logfile,
             self.backup_runfolder_logfile,
             self.proj_creation_script,
             self.runfolder_dx_run_script,
-            self.bcl2fastqlog_path
+            self.decision_support_tool_logfile,
+            self.samplesheet_validator_logfile,
+            self.bcl2fastqlog_path,
         ]
 
-    def set_rf_logfiles(self):
-        """"""
+    def set_rf_logfiles(self) -> None:
+        """
+        Add runfolder log files as class atributes
+            :return None:
+        """
         for logfile, path in return_rflog_config(self.runfolder_name).items():
             setattr(self, logfile, path)
 
-        
-        # setattr(
-        #     self, "runfolder_dx_run_script",
-        #     ad_config.RFLOG_CONFIG["dx_run_script"] % self.runfolder_name
-        # )
-        # self. = (
-        # )
-        # self.congenica_dx_run_script = (
-        #     ad_config.RFLOG_CONFIG["congenica_upload_script"] % self.runfolder_name
-        # )
-        # self.proj_creation_script = (
-        #     ad_config.RFLOG_CONFIG["proj_creation_script"] % self.runfolder_name
-        # )
-        # self.backup_runfolder_logfile = (
-        #     ad_config.RFLOG_CONFIG["backup"] % self.runfolder_name
-        # )
-        # self.decision_support_tool_logfile = (
-        #     ad_config.RFLOG_CONFIG["decision_support_script_log"] % self.runfolder_name
-        # )
-        # self.upload_runfolder_logfile = (
-        #     ad_config.RFLOG_CONFIG["usw"] % self.runfolder_name
-        # )
-        # self.demultiplex_runfolder_logfile = (
-        #     ad_config.RFLOG_CONFIG["demultiplex_script_logfile"] % self.runfolder_name
-        #     )
-
-    def requires_processing(self) -> bool:
-        """
-        Calls other methods to determine whether the runfolder requires processing
-        (demultiplexing has finished successfully and the runfolder has not already been
-        uploaded)
-            :return bool:  Returns true if runfolder requires processing, else False
-        """
-        if (self.has_demultiplexed() and not self.already_uploaded()):
-            self.script_logger.info(
-                self.script_logger.log_msgs["runfolder_requires_proc"],
-                self.runfolder_name,
-                extra={"flag": self.script_logger.log_flags["info"] % "usw"},
-            )
-            return True
-        else:
-            self.script_logger.info(
-                self.script_logger.log_msgs["runfolder_prev_proc"],
-                self.runfolder_name,
-                extra={"flag": self.script_logger.log_flags["info"] % "usw"},
-            )
-            return False
-
-    def already_uploaded(self) -> bool:
-        """
-        Checks for presence of upload agent logfile (denotes that the runfolder has
-        already been processed).
-            :return (bool):     Returns True if runfolder already uploaded, else False
-        """
-        if os.path.isfile(self.upload_agent_logfile):
-            self.script_logger.info(
-                self.script_logger.log_msgs["ua_file_present"],
-                extra={"flag": self.script_logger.log_flags["info"] % "usw"},
-            )
-            return True
-        else:
-            # If file doesn't exist return false to continue, write to log file
-            self.script_logger.info(
-                self.script_logger.log_msgs["ua_file_absent"],
-                extra={"flag": self.script_logger.log_flags["info"] % "usw"},
-            )
-            return False
-
-    def has_demultiplexed(self) -> bool:
-        """
-        Check if demultiplexing has already been performed and completed sucessfully
-        Checks the demultiplex log file exists, and if present checks the expected
-        success string is in the last line of the log file.
-            :return (bool):     Return True if runfolder already demultiplexed, else
-                                False
-        """
-        if os.path.isfile(self.bcl2fastqlog_path):
-            with open(self.bcl2fastqlog_path, "r", encoding="utf-8") as logfile:
-                logfile_list = logfile.readlines()
-                completed_strs = [
-                    ad_config.STRINGS['demultiplexlog_tso500_msg'],
-                    ad_config.STRINGS['demultiplex_success']
-                ]
-                if any(
-                    re.search(success_str, logfile_list[-1])
-                    for success_str in completed_strs
-                ):
-                    self.script_logger.info(
-                        self.script_logger.log_msgs["demux_complete"],
-                        extra={"flag": self.script_logger.log_flags["info"] % "usw"},
-                    )
-                    return True
-                else:
-                    self.script_logger.info(
-                        self.script_logger.log_msgs["demux_failed"],
-                        extra={"flag": self.script_logger.log_flags["info"] % "usw"},
-                    )
-                    return False
-        else:
-            # Write to logfile that not yet demultiplexed
-            self.script_logger.info(
-                self.script_logger.log_msgs["not_yet_demultiplexed"],
-                extra={"flag": self.script_logger.log_flags["info"] % "usw"},
-            )
-            return False
-
-    def add_runfolder_loggers(self):
+    def add_runfolder_loggers(self) -> None:
         """
         Add runfolder loggers to runfolder object
+            :return None:
         """
-        setattr(self, 'rf_loggers', ad_logger.AdLoggers(self.logfiles_config))
+        setattr(self, 'rf_loggers', ad_logger.RunfolderLoggers(self.logfiles_config))
+
+    def add_runfolder_logger(self, logger_name) -> None:
+        """
+        Add a single runfolder logger to runfolder object
+            :return None:
+        """
+        logfile_config = {logger_name: self.logfiles_config[logger_name]}
+        setattr(self, 'rf_loggers', ad_logger.RunfolderLoggers(logfile_config))
